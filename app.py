ab09956000c5fa10e633d90fb54f641deecfa0c0
nnnalyd <84059521+nnnalyd@users.noreply.github.com>
2024-03-23 21:15:17 +1100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, redirect, request, jsonify
+import spotifyAPI as s
+import os
+import urllib.parse
+import requests
+from datetime import *
+import json
+
+app = Flask(__name__)
+
+REDIRECT_URI = "http://127.0.0.1:5000/callback"
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+token = s.get_token()
+
+newRelease = s.newReleases(token)
+search = s.search_for_artist(token, 'frank ocean')
+
+session = {}
+
+
+@app.route('/')
+def index():
+   return "Welcome to Spotify <a href ='/login'> login with spotify</a>"
+
+   
+@app.route('/login')
+def login():
+   scope = "user-read-private user-read-email user-follow-read"
+   
+   params = {
+      'client_id' : client_id,
+      'response_type' : 'code',
+      'scope' : scope,
+      'redirect_uri' : REDIRECT_URI,
+      'show_dialog': True
+   }
+   
+   auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+   
+   return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+   if 'error' in request.args:
+      return jsonify({'error' : request.args['error']})
+   
+   if 'code' in request.args:
+      req_body = {
+         'code': request.args['code'],
+         'grant_type': "authorization_code",
+         'redirect_uri': REDIRECT_URI,
+         'client_id': client_id,
+         'client_secret': client_secret
+      }
+      
+   response = requests.post(TOKEN_URL, data=req_body)
+   token_info = response.json()
+   
+   session['access_token'] = token_info['access_token']
+   session['refresh_token'] = token_info['refresh_token']
+   session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']     
+   
+   return redirect('/home') 
+
+@app.route('/playlists')
+def get_playlists():
+   if 'access_token' not in session:
+      return redirect('/login')
+   
+   if datetime.now().timestamp() > session['expires_at']:
+      return redirect('/refresh-token')
+   
+   playlists = s.userPlaylists(session['access_token'])
+   
+   return jsonify(playlists)
+
+@app.route('/followed-artists')
+def followed_artists():
+   if 'access_token' not in session:
+      return redirect('/login')
+   
+   if datetime.now().timestamp() > session['expires_at']:
+      return redirect('/refresh-token')
+   
+   artists = s.userFollowedArtists(session['access_token'])
+   
+   return jsonify(artists)
+
+@app.route('/refresh-token')
+def refresh_token():
+   if 'refresh_token' not in session:
+      return redirect('/login')
+   
+   if datetime.now().timestamp() > session['expires_at']:
+      req_body = {
+         'grant_type': 'refresh_token',
+         'refresh_token': session['refresh_token'],
+         'client_id': client_id,
+         'client_secret': client_secret
+      }
+      
+   response = requests.post(TOKEN_URL, data=req_body)
+   new_token_info = response.json()
+   
+   session['access_token'] = new_token_info['access_token']
+   session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']     
+   
+   return redirect('/home')
+
+@app.route('/home')
+def home():
+   return render_template('home.html')
+
+#def home():
+   #return render_template('test.html', data=newRelease, dataS = search)
+   
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', debug=True)
---
+++ spotifyAPI.py
@@ -0,0 +1,150 @@
+#this whole module is needed to access the spotify api
+
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+#standard urls for api interaction
+API_URL = 'https://api.spotify.com/v1/'
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization" : "Bearer " + token}
+
+#searching for artist
+def search_for_artist(token, artist_name):
+    url = f"{API_URL}search"
+    headers = get_auth_header(token)
+    query  = f"?q={artist_name}&type=artist&limit=5"
+    #type is what we are looking for, basically filters
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)['artists']['items']
+    
+    dict = []
+    
+    length = int(len(json_result))
+    i = 0
+    
+    while i < length:
+        dict.append({
+            'artist' : json_result[i]['name'],
+            'id' : json_result[i]['id']
+        })
+        i += 1
+        
+    return dict
+
+#getting specified artist from api
+def getArtist(token):
+    url = f"{API_URL}artists/2h93pZq0e7k5yf4dywlkpM"
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
+
+def getRecommendations(token,artistid):
+    url = f"{API_URL}recommendations?limit=10&seed_artists={artistid}"
+    headers = get_auth_header(token)
+    
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)['tracks']
+    
+    dict = []
+    
+    length = int(len(json_result))
+    i = 0
+    
+    while i < length:
+        dict.append({
+            'track name' : json_result[i]['name'],
+            'artist name' : json_result[i]['artists'][0]['name']
+        })
+        i += 1
+        
+    return dict
+
+#getting newReleases from api
+def newReleases(token):
+    url = f"{API_URL}browse/new-releases?limit=3"
+    headers = get_auth_header(token)
+    
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    data = json_result['albums']['items']
+    
+    dict = []
+    
+    length = int(len(data))
+    i = 0
+    
+    while i < length:
+        dict.append({
+            'artist' : data[i]['artists'][0]['name'],
+            'album' : data[i]['name'],
+            'albumC' : data[i]['images'][0]['url']
+        })
+        i += 1
+        
+    return dict
+
+def userPlaylists(token):
+    url = f'{API_URL}me/playlists?limit=2'
+    headers = get_auth_header(token)
+    
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)['artists']['items']
+    
+    return json_result
+    
+def userFollowedArtists(token):
+    url = f'{API_URL}me/following?type=artist&limit=50'
+    headers = get_auth_header(token)
+    
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)['artists']['items']
+    
+    dict = []
+    
+    length = int(len(json_result))
+    i = 0
+    
+    while i < length:
+        dict.append({
+            'artist name' : json_result[i]['name'],
+        })
+        i += 1
+        
+    return dict
+
+if __name__ == "__main__":
+    token = get_token()
+    print(userPlaylists(token))
+    #term = input('Search Term: ')
+    #id = search_for_artist(token, term)[0]['id']
+    #print(id)
+    #print(getRecommendations(token,id))
