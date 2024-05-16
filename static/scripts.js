83331af0129d29931637b321b785ab4760034346
nnnalyd <84059521+nnnalyd@users.noreply.github.com>
2024-05-16 15:41:29 +1000

    commit
    js further editted

---
+++ static/scripts.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/user-recommendations')
+        .then(response => response.json())
+        .then(items => {
+            const itemContainer = document.getElementById('dict');
+            
+            dict.forEach(item => {
+                const itemDiv = document.getElementById('div');
+                const itemTitle = document.getElementById('h2');
+                const itemImg = document.getElementById('img');
+
+                if (item.type = 'artist'){
+                    itemTitle.textContent = item.artist_name;
+                    itemImg.src = item.img_url;
+
+                    itemDiv.appendChild(itemTitle);
+                    itemDiv.appendChild(itemImg)
+                }
+                if (item.type = 'track'){
+                    itemTitle.textContent = item.track_name;
+                    itemImg.src = item.img_url;
+                    const itemArtist = document.getElementById('h2');
+                    itemArtist.textContent = item.artist_name;
+                    const itemPreview = document.getElementById('audio');
+                    itemPreview.controls = true;
+                    itemPreview.src = item.preview_url;
+
+                    itemDiv.appendChild(itemTitle);
+                    itemDiv.appendChild(itemImg);
+                    itemDiv.appendChild(itemArtist);
+                    itemDiv.appendChild(itemPreview);
+                }
+            });
+        }
+        )
+})
--- templates/js/showitems.js
+++
@@ -1,9 +0,0 @@
-fetch('/user-recommendations')
-    .then(response => response.json())
-    .then(items => {
-        const itemDiv = document.getElementById('div');
-        const itemTitle = document.getElementById('h2');
-        
-    }
-
-    )
